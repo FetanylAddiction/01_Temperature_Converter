5db98d7a9c0fdfe70384d13ab12180b41065add6
FetanylAddiction <60630449+FetanylAddiction@users.noreply.github.com>
2021-02-11 12:01:28 +1300

    02_converter_GUI
    the second GUI part to this project

---
+++ 02_Converter_GUI_v1.py
@@ -0,0 +1,43 @@
+from tkinter import *
+from functools import partial   # To prevent unwanted windows
+import random
+
+
+class Converter:
+    def __init__(self):
+
+        # Formatting variables
+        background_color = "dim gray"
+
+        # Converter Frame
+        self.converter_frame = Frame(width=300, bg=background_color,
+                                     pady=10)
+        self.converter_frame.grid()
+
+        # Temperature Converter Heading (row 0)
+        self.temp_converter_label = Label(self.converter_frame, text="Temperature Converter",
+                                          font=("Arial", "16", "bold"),
+                                          padx=10, pady=10)
+        self.temp_converter_label.grid(row=0)
+
+        # User instructions (row 1)
+        self.temp_instructions_label = Label(self.converter_frame,
+                                          text="Type in the amount to be "
+                                                "converted and the push "
+                                               "one of the buttons below...",
+                                          font="Arial 10 italic", wrap=250,
+                                          justify=LEFT, bg=background_color,
+                                          padx=10, pady=10)
+        self.temp_instructions_label.grid(row=1)
+
+        # Temperature entry box (row 2)
+
+
+        # Conversion buttons frame (row 3)
+
+
+        # Answer label (row 4)
+
+
+# main rountine
+if __name__ == "__main__":
